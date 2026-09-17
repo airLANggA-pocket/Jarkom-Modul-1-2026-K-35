f6fb6b30b4de86876be7813b4f747d1e98825abe
airLANggA-pocket <stellar.tales24@gmail.com>
2026-09-18 00:45:48 +0700

    pls ini revisi terakhir dong

--- script/soal_8.sh
+++ script/soal_8.sh
@@ -2,9 +2,13 @@
 
 # Node Knights
 wget knights.zip "https://drive.google.com/drive/folders/1tvZpueSH9E3GWwXM6KNnM64Y5wNoIAYP?usp=sharing"
-lftp -u alice 10.81.2.10
+lftp -u alice,password_alice -e "put knights_report.txt; quit" 10.81.2.10
+
+unzip knights_report.zip
 
 ftp alice@10.81.2.10
 ~> password: alice
-~> 
+~> passive
+~> put knights_report.txt
 ~> exit
+~> EOF
